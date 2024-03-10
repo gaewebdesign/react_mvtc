9d546e2e154ef35d45a6325299daf22decf4d473
githubmutt@gmail.com <github.mutt@gmail.com>
2024-03-10 01:11:09 -0800

    react-reacter-dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+import {BrowserRouter, Routes,Route,Link} from "react-router-dom"
+
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
