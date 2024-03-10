e6c725bd7ebb0ea5f135a70b9bed776b50e738b1
githubmutt@gmail.com <github.mutt@gmail.com>
2024-03-10 01:19:32 -0800

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,48 @@ import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 
 import Card from './Card.jsx'
+import Members from './pages/Members.jsx'
+
+function About(){
+  return(
+   <h1>Hello React Router</h1>
+  )
+}
+
+function Home(){
+ return(
+  <h1>Home,  React Router</h1>
+ )
+}
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       
-       <h1>MVTC</h1>
+    < BrowserRouter>
+    <header>
+      
+    <nav className="navbar">
+   <Link to = "/">Home   </Link>&nbsp;|&nbsp;
+   <Link to = "/about">About </Link>&nbsp;|&nbsp;
+   <Link to = "/members">Members   </Link>
+   </nav>
+
+   </header>
+   <Routes>
+
+      <Route path="/" element={<Home/>}   />
+      <Route path="/about" element={<About/>}   />
+      <Route path="/members" element={<Members/>}   />
+
+      
+      
+      </Routes>
+   
+   
+   </BrowserRouter>  
   
   )
 
---
+++ src/pages/Members.jsx
@@ -0,0 +1,8 @@
+function Members(){
+    return(
+     <h1>Members page from pages/member.jsx</h1>
+    )
+  }    
+
+
+  export default Members
